f02d966d80db268a4b776a074fc81395785c513b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-31 17:47:17 +0100

    Conditionally renders a nice looking spineer

--- src/containers/PokemonsContainer.js
+++ src/containers/PokemonsContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
+import Spinner from 'react-spinner-material';
 
 import Pokemon from '../components/Pokemon';
 import { GET_POKEMONS } from '../graphql/get-pokemons';
@@ -12,7 +13,9 @@ export default () => {
   return (
     <div className="container">
       {loading ? (
-        <p>Loading...</p>
+        <div className="spinner">
+          <Spinner radius={120} color={'#333'} stroke={2} visible={true} />
+        </div>
       ) : (
         pokemons.map(pokemon => <Pokemon key={pokemon.key} pokemon={pokemon} />)
       )}
