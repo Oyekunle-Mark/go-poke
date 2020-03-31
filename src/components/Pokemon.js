aca9b0e90ca2c8a6110e968c3a76810b7d32bd86
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-31 14:06:49 +0100

    Fixed a typo

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -12,7 +12,7 @@ export default ({ pokemon }) => (
     <div className="pokemon__image">
       <img src={pokemon.image} alt={pokemon.name} />
     </div>
-    <div className="pokemon__attack">
+    <div className="pokemon__attacks">
       {pokemon.attacks.special.slice(0, 3).map(attack => (
         <span key={`${attack.name}-${attack.damage}`}>{attack.name}</span>
       ))}
