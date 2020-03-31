225668f816c33b45d39d492102f0fb686e544a11
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-31 17:45:37 +0100

    Shows a loading while loading

--- src/containers/PokemonsContainer.js
+++ src/containers/PokemonsContainer.js
@@ -5,16 +5,17 @@ import Pokemon from '../components/Pokemon';
 import { GET_POKEMONS } from '../graphql/get-pokemons';
 
 export default () => {
-  const { loading, error, data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
+  const { loading, data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
     variables: { first: 10 },
   });
 
   return (
     <div className="container">
-      {pokemons &&
-        pokemons.map(pokemon => (
-          <Pokemon key={pokemon.key} pokemon={pokemon} />
-        ))}
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        pokemons.map(pokemon => <Pokemon key={pokemon.key} pokemon={pokemon} />)
+      )}
     </div>
   );
 };
