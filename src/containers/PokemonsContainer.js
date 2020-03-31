9e32ac96e38fb3233496a787cc7575f422b9a25b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-31 17:40:47 +0100

    destructured the loading and error properties from the use query

--- src/containers/PokemonsContainer.js
+++ src/containers/PokemonsContainer.js
@@ -5,7 +5,7 @@ import Pokemon from '../components/Pokemon';
 import { GET_POKEMONS } from '../graphql/get-pokemons';
 
 export default () => {
-  const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
+  const { loading, error, data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
     variables: { first: 10 },
   });
 
